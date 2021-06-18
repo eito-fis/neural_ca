abb1f77dce4a12848c7edda9975918ca1ff5cea0
Eamon Ito-Fisher <eamon@itofisher.com>
2021-06-17 21:57:58 -0700

    UPDATE: Refactor SamplePool to be easier to subclass

--- neural_ca/pools/sample_pool.py
+++ neural_ca/pools/sample_pool.py
@@ -4,19 +4,16 @@ import tensorflow as tf
 from neural_ca import util
 
 class SamplePool:
-    def __init__(self, pool_size, state_size, emoji):
+    def __init__(self, pool_size, state_size, data):
         assert pool_size > 0 and state_size > 0
-        self.target = util.image.load_emoji(emoji)
-        self.shape = self.target.shape
         self.pool_size = pool_size
         self.state_size = state_size
-        self.pool = self.build_seeds(pool_size)
+        self.data = self.build_data(data)
+        self.shape = self.build_shape()
+        self.pool = self.build_pool()
 
     def sample(self, batch_size):
-        assert batch_size <= self.pool_size, "Batch size larger than pool size."
-        idxs = tf.range(0, self.pool_size)
-        sample_idxs = tf.random.shuffle(idxs)[:batch_size]
-        sample = tf.gather(self.pool, sample_idxs, axis=0)
+        sample, sample_idxs = self.build_sample(batch_size)
 
         # Replace one sampled seed with a fresh seed
         fresh_seed = self.build_seeds(1)
@@ -37,6 +34,25 @@ class SamplePool:
             new_seeds,
         )
 
+    def build_data(self, data):
+        return util.image.load_emoji(data)
+
+    def build_shape(self):
+        return self.data.shape
+
+    def build_pool(self):
+        return self.build_seeds(self.pool_size)
+
+    def build_sample(self, batch_size):
+        assert batch_size <= self.pool_size, "Batch size larger than pool size."
+        idxs = tf.range(0, self.pool_size)
+        sample_idxs = tf.random.shuffle(idxs)[:batch_size]
+        sample = tf.gather(self.pool, sample_idxs, axis=0)
+        return sample, sample_idxs
+
+    def build_target(self):
+        return self.data
+
     def build_seeds(self, batch_size):
         """ Makes batch of seeds """
         height, width = self.shape[:2]
@@ -44,6 +60,3 @@ class SamplePool:
         seeds[:, height // 2, width // 2, 3:] += 1
         seeds = tf.convert_to_tensor(seeds, dtype=tf.float32)
         return seeds
-
-    def build_target(self):
-        return self.target
