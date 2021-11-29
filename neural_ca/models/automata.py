b8ecaf64d66a1770ee6ef3c5e28aee8e29a2d532
eito-fis <eamon@itofisher.com>
2021-11-29 14:08:22 -0500

    ADD: Update hyperparams

--- neural_ca/models/automata.py
+++ neural_ca/models/automata.py
@@ -23,7 +23,7 @@ class AutomataModel(tf.keras.Model):
         self.filters = self.build_filters()
         self.filters.trainable = False
         self.dense = tf.keras.layers.Conv2D(
-            filters=48,
+            filters=96,
             kernel_size=1,
             padding="Same",
             activation=tf.keras.activations.relu
